74c2f70833ab5667150bb63624c47c110a732bc9
Michael Holzmann <m.holzmann98@gmx.de>
2020-10-02 17:06:06 +0200

    IApp INetDNetApp

---
+++ source/de/inet-dnet-corp/inet-dnet/app/IApp.java
@@ -0,0 +1,10 @@
+package inetdnetcorp.inetdnet.app;
+
+
+public interface IApp{
+
+	public void showUI();
+	//public Logger getLogger();
+	public String getAppGUID();
+
+}
---
+++ source/de/inet-dnet-corp/inet-dnet/app/INetDNetApp.java
@@ -0,0 +1,14 @@
+package inetdnetcorp.inetdnet.app;
+
+public class INetDNetApp implements IApp{
+
+		public void showUI(){}
+		//public Logger getLogger(){}
+		public String getAppGUID(){
+		
+			// TODO: return Guid
+			return "";
+
+		}
+
+}
