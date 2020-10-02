ebf1ae3c26f7d9ed549d946fe6ebad6d13609dd4
Michael Holzmann <m.holzmann98@gmx.de>
2020-10-02 16:56:55 +0200

    AppRunner

---
+++ source/de/inet-dnet-corp/inet-dnet/AppRunner.java
@@ -0,0 +1,17 @@
+package de.inetdnetcorp.inetdnet;
+
+public class AppRunner{
+
+	public static void main(String[] args){
+	
+		(new AppRunner()).run();
+
+	}
+
+	public void run(){
+	
+		
+
+	}
+
+}
